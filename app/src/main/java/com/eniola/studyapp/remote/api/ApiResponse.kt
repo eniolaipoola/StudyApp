825da8b7d8241e0d7587b54e5ff9bbd98fb85346
Ipoola Eniola <eniolaipoola@gmail.com>
2021-01-19 17:38:55 +0100

    create network helper class

---
+++ app/src/main/java/com/eniola/studyapp/remote/api/ApiResponse.kt
@@ -0,0 +1,13 @@
+package com.eniola.studyapp.remote.api
+
+/**
+ * Copyright (c) 2021 Eniola Ipoola
+ * All rights reserved
+ * Created on 19-Jan-2021
+ */
+
+data class StudyAppApiResponse<D>(
+    val status: String,
+    val data : D,
+    val message: String
+)
