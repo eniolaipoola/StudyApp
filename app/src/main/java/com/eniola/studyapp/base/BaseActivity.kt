d92b4979266a017cc04a36cd8afc0617b7e953fe
Ipoola Eniola <eniolaipoola@gmail.com>
2021-01-19 03:07:43 +0100

    setup project dependencies

---
+++ app/src/main/java/com/eniola/studyapp/base/BaseActivity.kt
@@ -0,0 +1,10 @@
+package com.eniola.studyapp.base
+
+/**
+ * Copyright (c) 2021 Eniola Ipoola
+ * All rights reserved
+ * Created on 19-Jan-2021
+ */
+
+open class BaseActivity {
+}
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven (url = "https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
+}
